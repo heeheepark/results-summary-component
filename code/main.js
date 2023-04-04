2de7d5465fd9a7b4e01c53b8a3a6d3a3b311b734
heeheepark <pjh9641@gmail.com>
2023-04-04 15:44:18 +0900

    아이콘 추가

--- code/main.js
+++ code/main.js
@@ -1,25 +1,25 @@
 let data = [
-  {
-    category: "Reaction",
-    score: 80,
-    icon: "./assets/images/icon-reaction.svg"
-  },
-  {
-    category: "Memory",
-    score: 92,
-    icon: "./assets/images/icon-memory.svg"
-  },
-  {
-    category: "Verbal",
-    score: 61,
-    icon: "./assets/images/icon-verbal.svg"
-  },
-  {
-    category: "Visual",
-    score: 72,
-    icon: "./assets/images/icon-visual.svg"
-  }
-]
+    {
+      "category": "Reaction",
+      "score": 80,
+      "icon": "../results-summary-component-main/assets/images/icon-reaction.svg"
+    },
+    {
+      "category": "Memory",
+      "score": 92,
+      "icon": "../results-summary-component-main/assets/images/icon-memory.svg"
+    },
+    {
+      "category": "Verbal",
+      "score": 61,
+      "icon": "../results-summary-component-main/assets/images/icon-verbal.svg"
+    },
+    {
+      "category": "Visual",
+      "score": 72,
+      "icon": "../results-summary-component-main/assets/images/icon-visual.svg"
+    }
+  ]
 
 let scoreCategory = [];
 let score = [];
@@ -27,15 +27,18 @@ let icons = [];
 let scoreTitle = document.getElementsByClassName('scoreTitle');
 let scoreText = document.getElementsByClassName('score');
 let totalScore = document.getElementsByClassName('totalScore');
+let iconImage = document.getElementsByClassName('icon');
 
 for(let i = 0; i < data.length; i++) {
   scoreCategory.push(data[i].category);
   score.push(data[i].score);
+  icons.push(data[i].icon);
 }
 
 for(let i = 0; i < scoreCategory.length; i++) {
   scoreTitle[i].textContent = scoreCategory[i];
   scoreText[i].textContent = score[i];
+  iconImage[i].src = icons[i];
 }
 
 function average(score) {
