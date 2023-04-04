0799703ab82f9baa51488e489529cfcd6bfd8b5b
heeheepark <pjh9641@gmail.com>
2023-04-04 12:33:47 +0900

    점수 이름, 숫자 자바스크립트 추가

--- code/main.js
+++ code/main.js
@@ -20,3 +20,37 @@ let data = [
     icon: "./assets/images/icon-visual.svg"
   }
 ]
+
+let scoreCategory = [];
+let score = [];
+let icons = [];
+let scoreTitle = document.getElementsByClassName('scoreTitle');
+let scoreText = document.getElementsByClassName('score');
+let totalScore = document.getElementsByClassName('totalScore');
+
+for(let i = 0; i < data.length; i++) {
+  scoreCategory.push(data[i].category);
+  score.push(data[i].score);
+}
+
+for(let i = 0; i < scoreCategory.length; i++) {
+  scoreTitle[i].textContent = scoreCategory[i];
+  scoreText[i].textContent = score[i];
+}
+
+function average(score) {
+  let sum = 0;
+
+  for(let i = 0; i <score.length; i++) {
+    sum += score[i]
+  }
+  return sum / score.length; 
+}
+
+totalScore[0].textContent = Math.floor(average(score));
+
+
+
+
+
+
